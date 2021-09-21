75e499dfd2e5e25ac78c747cf95fcbd54f33aba0
Celina Tala <59627268+celinatala-1@users.noreply.github.com>
2021-09-20 23:12:17 -0400

    Final Push

---
+++ Scrum/Scrum/MeetingView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Scrum
+//
+//  Created by Celina Tala on 9/15/21.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    var player: AVPlayer { AVPlayer.sharedDingPlayer }
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.color)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, scrumColor: scrum.color)
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
+        .onAppear {
+            scrumTimer.speakerChangedAction = {
+                            player.seek(to: .zero)
+                            player.play()
+                        }
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrumTimer.secondsElapsed / 60)
+                        scrum.history.insert(newHistory, at: 0)
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.data[0]))
+    }
+}
+
+
--- Scrum/Scrum/ScrumApp.swift
+++ Scrum/Scrum/ScrumApp.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 @main
-struct ScrumApp: App {
+struct ScrumdingerApp: App {
+    @ObservedObject private var data = ScrumData()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ScrumsView(scrums: $data.scrums) {
+                    data.save()
+                }
+            }
+            .onAppear {
+                data.load()
+            }
         }
     }
 }
