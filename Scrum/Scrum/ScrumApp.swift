7f261a453b5d97ee84feefa6755bad611591031b
Celina Tala <59627268+celinatala-1@users.noreply.github.com>
2021-09-20 23:05:16 -0400

    Testing

---
+++ Scrum/Scrum/ScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumApp.swift
+//  Scrum
+//
+//  Created by Celina Tala on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
